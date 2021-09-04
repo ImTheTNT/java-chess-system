eefad5de77f3129ef53d57692d4a2e8219f00355
ImTheTNT <renatotbreis@gmail.com>
2021-09-04 19:02:24 -0300

    added project setup / first class

---
+++ src/application/Program.java
@@ -0,0 +1,12 @@
+package application;
+
+import board.Position;
+
+public class Program
+{
+  public static void main (String [] args)
+  {
+    Position pos = new Position (11, 4);
+    System.out.println (pos);
+  }
+}
